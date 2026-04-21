3500c98f179e488ab86970d254bb649b14843f1e
Think2twice <youchailoki@gmail.com>
2026-04-21 22:26:58 +0800

    Add new function to branch gitbro2

---
+++ Hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    printf("Sum = %d\n", sum);
+    
+    return 0;
+}
